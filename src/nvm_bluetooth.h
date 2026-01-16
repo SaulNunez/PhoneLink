2b19a2dd4ae116beab4b7962ca7aa789c9993c1d
Saul Nunez <saul.nunez99@gmail.com>
2026-01-15 22:01:50 -0700

    Comments for functions for saving bluetooth information to NVS

--- src/nvm_bluetooth.h
+++ src/nvm_bluetooth.h
@@ -1,7 +1,20 @@
 #ifndef __NVM_BLUETOOTH__
 #define __NVM_BLUETOOTH__
 
+/**
+ * @brief Commit any pending changes to NVS.
+ *
+ * This function commits any changes made to the NVS namespace opened by init_nvs().
+ */
 void commit_nvs_changes();
+
+/**
+ * @brief Initialize Non-Volatile Storage (NVS).
+ *
+ * This function initializes the NVS flash partition and opens a handle to the
+ * "bluetooth" namespace. If the NVS partition is truncated or a new version is found,
+ * it attempts to erase and re-initialize.
+ */
 void init_nvs();
 
 #endif /* __NVM_BLUETOOTH__*/
